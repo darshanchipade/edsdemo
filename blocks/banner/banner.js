9252e9ccdd000d633ab75fede157e46476244765
darshanchipade <dchipade@gmail.com>
2025-03-21 16:35:41 -0700

    Update banner.js

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -6,11 +6,11 @@ export default function decorate(block) {
   const banner = document.createElement('div');
   banner.className = 'banner';
 
-  banner.innerHTML = `
+  banner.innerHTML = '
     <img src="${imageCell.textContent.trim()}" alt="Banner">
     <h2>${textCell.textContent.trim()}</h2>
     <a href="${linkCell.textContent.trim()}">${btnTextCell.textContent.trim()}</a>
-  `;
+ ' ;
 
   block.innerHTML = '';
   block.appendChild(banner);
